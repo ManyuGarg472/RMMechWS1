24b6d53adb4e44461864fee8ac9fae4d7affada2
ManyuGarg472 <86672826+ManyuGarg472@users.noreply.github.com>
2021-06-29 23:42:58 +0530

    Add files via upload

---
+++ ASCII value.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+char ch; // variable declaration
+printf("Enter a character: ");
+scanf("%c",&ch); // user input
+printf("\n The ascii value of the ch variable is : %d", ch);
+printf("\n Manyu Garg _ 200929070_ 28/05/2021");
+return 0;
+}
